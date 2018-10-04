f216f90b0c506aaf41e3ef478c6eb8583f5be4d9
Full Bright <full3right@gmail.com>
2018-10-04 13:26:49 +0200

    Add xml file load to test case

--- processors/test_margin_notes_processor.py
+++ processors/test_margin_notes_processor.py
@@ -1,8 +1,19 @@
 import unittest
-
 import margin_notes_processor
+import os
 
-class Test_margin_notes_processor:
+BIBLE_FILE = os.path.join(os.path.dirname(__file__), '../corpus/French.xml')
+
+class Test_margin_notes_processor(unittest.TestCase):
+
+    def setUp():
+       # Load the french xml bible file
+        self.bibledata = open(BIBLE_FILE).read()
 
     def test_merge_notes():
         # Test if notes are merged correctly
+
+    def test_split_chapters():
+        # Test that two chapters in the xml flow are splitted correctly
+
+
