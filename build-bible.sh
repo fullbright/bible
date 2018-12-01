490cf642df872f383d3906fc9e6689930c65658f
Full Bright <full3right@gmail.com>
2018-12-01 22:19:11 +0100

    Fix books compilation

--- build-bible.sh
+++ build-bible.sh
@@ -1,7 +1,19 @@
 echo "Start bible building"
 
 mkdir _build
-pdflatex -interaction=nonstopmode -output-directory _build projects/bible_fra_tufte/bible_fra_tufte.tex
-pdflatex -interaction=nonstopmode -output-directory _build projects/bible_fra_tufte/bible_fra_tufte_full.tex
+
+echo "Cleaning the _build folder in case it existed"
+rm -rf _build/**
+
+cd projects/
+
+pdflatex -interaction=nonstopmode -output-directory ../_build bible_fra_tufte/bible_fra_tufte.tex
+pdflatex -interaction=nonstopmode -output-directory ../_build bible_fra_tufte/bible_fra_tufte_full.tex
+#pdflatex -interaction=nonstopmode -output-directory ../_build bible/bible.tex
+#pdflatex -interaction=nonstopmode -output-directory ../_build bible_fra/bible_fra.tex
+#pdflatex -interaction=nonstopmode -output-directory ../_build bible_fra_short/bible_fra_short.tex
+#pdflatex -interaction=nonstopmode -output-directory ../_build kjv/bible_fra_short.tex
+
+cd ..
 
 echo "Build building is finished."
